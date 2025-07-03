53a3212c657382405de6520cc73b1745f8cbd1b6
oozye1 <jamesfairfoul@gmail.com>
2025-07-03 09:09:21 +0100

    reverted to the 2d google map - currently working - one flag location button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,13 +48,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.gms:play-services-base:18.5.0")
-    // ARCore
-    implementation("com.google.ar:core:1.31.0")
-    // Community-maintained Sceneform
-    implementation("com.gorisse.thomas.sceneform:ux:1.23.0") // Correct artifact name
+    // ARCore and Sceneform dependencies REMOVED
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
-
-
